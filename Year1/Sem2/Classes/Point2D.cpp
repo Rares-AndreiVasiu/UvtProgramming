c5f71d3f86301bf4f7da2bfdda320e668a3d96da
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-04-29 22:24:13 +0300

    Update Point2D.cpp

--- Year1/Sem2/Classes/Point2D.cpp
+++ Year1/Sem2/Classes/Point2D.cpp
@@ -4,20 +4,43 @@
 
 Point2D::Point2D(int x, int y)
 {
-    this -> x = x;
+    this -> x = new int;
 
-    this -> y = y;
+    this -> y = new int;
+    
+    *(this -> x) = x;
+
+    *(this -> y) = y;
+}
+
+Point2D::Point2D(const Point2D& other)
+{
+    this -> x = new int;
+
+    this -> y = new int;
+
+    *(this -> x) = *(other.x);
+
+    *(this -> y) = *(other.y);
 }
 
 int Point2D::getX()
 {
-    return this -> x;
+    return *(this -> x);
 }
 
 int Point2D::getY()
 {
-    return this -> y;
+    return *(this -> y);
 }
+
+Point2D::~Point2D()
+{
+    delete this -> x;
+
+    delete this -> y;
+}
+
 
 int main()
 {
@@ -25,7 +48,12 @@ int main()
 
     std::cin >> x >> y;
     
-    Point2D p1 = Point2D(x,y);
+    Point2D p1{x,y};
+    {
+        Point2D p2(p1);
+    }
     
     std::cout << p1.getX() << " " << p1.getY() << std::endl;
+
+    return 0;
 }
