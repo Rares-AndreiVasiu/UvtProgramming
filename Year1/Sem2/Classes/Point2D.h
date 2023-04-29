a35fbd9be5a72a0c2a6039781c8b5e3dfc721d91
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-04-29 22:24:22 +0300

    Update Point2D.h

--- Year1/Sem2/Classes/Point2D.h
+++ Year1/Sem2/Classes/Point2D.h
@@ -1,16 +1,21 @@
 #ifndef POINT2D_H
 #define POINT2D_H
 
-class Point2D 
+class Point2D
 {
     public:
-        Point2D(int, int);
-        int getX();
-        int getY();
+       Point2D(int, int);
 
+       Point2D(const Point2D&);
+       
+       ~Point2D();
+       
+       int getX();
+       
+       int getY();
+    
     private:
-        int x;
-        int y;
+       int *x, *y;
 };
 
 #endif
