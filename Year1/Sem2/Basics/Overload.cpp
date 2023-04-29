3820f88536c7c4755bf7d9d2f045b50b0370c2be
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-04-29 22:54:17 +0300

    Create Overload.cpp

---
+++ Year1/Sem2/Basics/Overload.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int min(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+float min(float a, float b)
+{
+    return a < b ? a : b;
+}
+
+bool min(bool a, bool b)
+{
+    return a < b ? a : b;
+}   
