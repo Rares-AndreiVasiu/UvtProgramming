edbc2376b694e47abf6a49e30758335110cc2627
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-04-30 17:09:18 +0300

    Create counter.h

---
+++ Year1/Sem2/Homework/h1/counter.h
@@ -0,0 +1,20 @@
+#ifndef COUNTER_H
+#define COUNTER_H
+
+class counter
+{
+private:
+    int cnt;
+public:
+    counter();
+
+    void increase();
+
+    void print();
+
+    void reset();
+};
+
+
+
+#endif
