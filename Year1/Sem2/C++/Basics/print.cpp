a92d03408f8752ee24679512a99e4680c7723256
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2023-05-02 22:58:52 +0300

    Create print.cpp

---
+++ Year1/Sem2/C++/Basics/print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string _name;
+    
+    int x;
+
+    double y;
+
+    std::cin >> _name >> x >> y;
+
+    std::cout << _name << ' ' << x << ' ' << y;
+
+    return 0;
+}
